937ae3a9d51252587e092323e8d8c448e135e0d3
Greg Palmier <gregpalmier@gmail.com>
2014-08-19 15:53:33 -0500

    rubocop offenses

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,22 +23,22 @@
 end
 
 template "#{node['memcached']['server_config']}" do
- source "sysconfig.conf.erb"
- owner "root"
- group 0
- mode 00644
- notifies :restart, "service[memcached]", :immediately
+  source 'sysconfig.conf.erb'
+  owner 'root'
+  group 0
+  mode 00644
+  notifies :restart, 'service[memcached]', :immediately
 end
 
 template "#{node['memcached']['service_config']}" do
- source "memcached.conf.erb"
- owner "root"
- group 0
- mode 00644
- notifies :restart, "service[memcached]", :immediately
+  source 'memcached.conf.erb'
+  owner 'root'
+  group 0
+  mode 00644
+  notifies :restart, 'service[memcached]', :immediately
 end
 
 service 'memcached' do
-  supports :status => true, :restart => true, :stop => true
-  action [ :enable, :start ]
+  supports status: 'true', restart: 'true', stop: 'true'
+  action [:enable, :start]
 end
